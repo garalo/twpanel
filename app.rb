b2228591fc0128a6e127e32539f5adb2812a7af6
garalo <ahmedkilic@gmail.com>
2018-08-23 20:34:33 +0900

    key değitim ak adem oldur

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ require 'omniauth-twitter'
 configure do
   enable :sessions
   use OmniAuth::Builder do
-    provider :twitter, 'FMY49JTN57AiLxqtgbE4push7', 'MSv1ZdYRAJoq6RDcKGIEftqWtxrDoxntEQAj2l5dif7LVrrorp', callback_url: "http://127.0.0.1:4567/auth/twitter/callback" # use ENV variables instead :)
+    #_ak_adem için keys
+    provider :twitter, '1TEwFFzAyfDGoqKgs1kenF6Nh', 'IDUcFEFaN9J9eFgQBLCYfQ9R1bvfvgUyfROG4uMzI2MmoRNCtm', callback_url: "http://127.0.0.1:4567/auth/twitter/callback" # use ENV variables instead :)
   end
 end
 
